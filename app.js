0f265accd50a12f08f4a66c5918c1f38d94b6367
Vedrana <vedrana_ravlic16@yahoo.com>
2018-10-24 23:44:30 +0200

    Added calculating func

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 let clear = false;
 let enteredNumbers = "";
-let button = document.querySelector(".button");
-let textbox = document.querySelector(".textbox");
 
-document.onload(() => {
-  button.addEventListener("click", calculateValue());
+window.addEventListener("DOMContentLoaded", () => {
+  let buttons = document.querySelectorAll(".btn");
+  let textbox = document.getElementById("textbox");
+
+  buttons.forEach(butt =>
+    butt.addEventListener("click", () => {
+      let val = butt.value;
+
+      if (
+        parseInt(val) == val ||
+        val === "." ||
+        val === "/" ||
+        val === "*" ||
+        val === "-" ||
+        val === "+" ||
+        val === "%"
+      ) {
+        if (clear === false) {
+          enteredNumbers += val;
+          textbox.value = enteredNumbers;
+          textbox.innerText = textbox.value;
+        } else {
+          enteredNumbers = val;
+          textbox.value = enteredNumbers;
+          clear = false;
+        }
+      }
+
+      if (val === "CE") {
+        enteredNumbers = "";
+        textbox.value = "";
+      } else if (val === "C") {
+        enteredNumbers = enteredNumbers.slice(0, -1);
+        textbox.value = enteredNumbers;
+      } else if (val === "=") {
+        textbox.value = eval(enteredNumbers);
+        clear = true;
+      }
+    })
+  );
 });
