be354117b7554f54876484a5fbab29a4f9a08593
Vedrana <vedrana_ravlic16@yahoo.com>
2018-10-26 22:44:55 +0200

    Added comments to app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let clear = false;
 let enteredNumbers = "";
 
+//Added event listener to check if dom content loaded
 window.addEventListener("DOMContentLoaded", () => {
   let buttons = document.querySelectorAll(".btn");
   let textbox = document.getElementById("textbox");
 
+  //Event listeners for every button
   buttons.forEach(butt =>
     butt.addEventListener("click", () => {
       let val = butt.value;
@@ -30,13 +32,14 @@ window.addEventListener("DOMContentLoaded", () => {
       }
 
       if (val === "CE") {
+        //Deletes the input
         enteredNumbers = "";
         textbox.value = "";
       } else if (val === "C") {
-        enteredNumbers = enteredNumbers.slice(0, -1);
+        enteredNumbers = enteredNumbers.slice(0, -1); //Deletes the last character
         textbox.value = enteredNumbers;
       } else if (val === "=") {
-        textbox.value = eval(enteredNumbers);
+        textbox.value = eval(enteredNumbers); //Calculates the input
         clear = true;
       }
     })
